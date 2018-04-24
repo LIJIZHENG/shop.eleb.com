3b3df9ac7fdd0b4b9bb60a532cede4918fc273c0
root <3436803285qq.com>
2018-04-24 22:00:10 +0800

    活动列表

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -13,12 +13,6 @@ class ActivityController extends Controller
     public function index(){
 //        $obj=Activity::all();
         $rows=DB::table('activities')->select()->get();
-//        $rows=(object)null;
-//        foreach ($obj as $value){
-//            if(strtotime($value->end)>=strtotime(date('Y-m-d',time()))){
-//                $rows=$value;
-//            }
-//        }
         return view('activity.index',compact('rows'));
     }
 }
